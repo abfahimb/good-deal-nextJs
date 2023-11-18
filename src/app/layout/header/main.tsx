97bdbedb1beee7c2b92e45232efed0bce542eb93
abdullah <abfahimb@gmail.com>
2023-11-18 07:02:14 +0600

    [#3] testing cicd

--- src/app/layout/header/main.tsx
+++ src/app/layout/header/main.tsx
@@ -11,7 +11,7 @@ const HeaderMain = () => {
         { name: 'Calendar', href: '#', current: false },
     ]
 
-    function classNames(...classes) {
+    function classNames(...classes: string[]) {
         return classes.filter(Boolean).join(' ')
     }
 
